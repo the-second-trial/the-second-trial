ecefc89b52a829f507716dc09b88ece90c1a91eb
Andrea <andry.tino@gmail.com>
2020-06-06 23:40:41 +0200

    Initialize the common solution

---
+++ src/lib/CommonLibrary/Animation/AnimationOrchestrator.cs
@@ -0,0 +1,22 @@
+﻿/// <summary>
+/// Andrea Tino - 2020
+/// </summary>
+
+namespace Animation
+{
+    using System;
+
+    /// <summary>
+    /// Orchestrates and manages animations.
+    /// </summary>
+    public class AnimationOrcherstrator
+    {
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="AnimationOrcherstrator"/> class.
+        /// </summary>
+        public AnimationOrcherstrator()
+        {
+        }
+    }
+}
